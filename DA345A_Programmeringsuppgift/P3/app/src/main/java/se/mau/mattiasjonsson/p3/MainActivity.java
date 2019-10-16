f8554121844962768279a8b6b5fe7bb65a6ba1f3
jonssonmattias <1jonssonmattias@gmail.com>
2019-10-16 11:45:35 +0200

    TEST

---
+++ DA345A_Programmeringsuppgift/P3/app/src/main/java/se/mau/mattiasjonsson/p3/MainActivity.java
@@ -0,0 +1,13 @@
+package se.mau.mattiasjonsson.p3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//TEST
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
