8bebd3eb23f2afb1138e0e1224810d1c8bd9b2f2
jonssonmattias <1jonssonmattias@gmail.com>
2020-01-15 17:50:31 +0100

    Update

--- DA387A_Assignment/TeamAssignment/app/src/main/java/se/mau/mattiasjonsson/teamassignment/MainActivity.java
+++ DA387A_Assignment/TeamAssignment/app/src/main/java/se/mau/mattiasjonsson/teamassignment/MainActivity.java
@@ -1,6 +1,7 @@
 package se.mau.mattiasjonsson.teamassignment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.hardware.Sensor;
@@ -12,9 +13,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -23,191 +27,29 @@ import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import se.mau.mattiasjonsson.teamassignment.views.Enemy;
+
 public class MainActivity extends AppCompatActivity {
-
-    private static boolean GAME_OVER = false;
-    private static int dy=7;
-
-    private Ball ball;
-    private ImageView ivBall;
-    private ArrayList<Enemy> enemies = new ArrayList<>();
-    private ArrayList<ImageView> ivEnemies = new ArrayList<>();
-    private HashMap<Enemy,Timer> timerList = new HashMap<>();
-    private ViewGroup layout;
-    private long lastLoopRun;
-    private SensorManager sensorManager;
-    private float xPos, xMax, yMax, xVel, startX;
+    private Button btn_startGame, btn_resumeGame;
+    private ListView leaderboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ivBall = findViewById(R.id.imageView2);
-        layout = (ConstraintLayout) findViewById(R.id.layout);
-
-        WindowManager wm = getWindowManager();
-        Display display = wm.getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        xMax = (float) size.x - 100;
-        yMax = (float) size.y;
-
-        showBall();
-        // moveBall();
-        //     loop();
-
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        sensorManager.registerListener(new BallSensorListener(), sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
-    }
-
-    @Override
-    protected void onStop() {
-        sensorManager.unregisterListener(new BallSensorListener());
-        super.onStop();
-    }
-
-    private void addEnemy(){
-        Timer enemyTimer = new Timer();
-        ImageView ivEnemy = new ImageView(this);
-        addContentView(ivEnemy, new ConstraintLayout.LayoutParams(75, 75));
-        String enemyName = "enemy"+new Random().nextInt(1000000);
-        Enemy enemy = new Enemy(enemyName);
-        timerList.put(enemy, enemyTimer);
-        enemies.add(enemy);
-        ivEnemies.add(ivEnemy);
-        showObstacle(ivEnemy, enemy);
-        moveObstacle(ivEnemy, enemyTimer);
-    }
-
-    private void showObstacle(ImageView ivEnemy, Enemy enemy){
-        ivEnemy.setY(40);
-        ivEnemy.setX(new Random().nextInt((int)xMax -ivEnemy.getWidth()));
-        ivEnemy.setBackgroundDrawable(enemy);
-    }
-
-    private void showBall(){
-        ball = new Ball(100, Color.BLACK);
-        Log.d("ballWidth", ball.getSize()+"");
-        startX = xMax/2-100;
-        ivBall.setY(700);
-        ivBall.setX(startX);
-        ivBall.setBackgroundDrawable(ball);
-    }
-
-    private void moveObstacle(final ImageView ivEnemy, Timer enemyTimer){
-        enemyTimer.schedule(new TimerTask() {
+        initComponents();
+        btn_startGame.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    changeObstaclePos(ivEnemy);
-                }
-            });
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
             }
-        },0, 20);
+        });
     }
 
-    private void changeObstaclePos(ImageView ivEnemy){
-        int enemyY = (int)ivEnemy.getY();
-        enemyY +=dy;
-        ivEnemy.setY(enemyY);
-        if(ivEnemy.getY()> yMax) {
-            removeEnemy(ivEnemy);
-        }
-    }
-
-    private boolean checkCollisions(){
-        for(ImageView ivEnemy: ivEnemies)
-            if(intersects(ivBall, ivEnemy))
-                return true;
-        return false;
-    }
-
-    private void removeEnemy(ImageView ivEnemy){
-        Enemy enemy = (Enemy)ivEnemy.getDrawable();
-        timerList.remove(enemy);
-        enemies.remove(enemy);
-        layout.removeView(ivEnemy);
-    }
-
-    private boolean isOutOfBounds(){
-        float ballX = ivBall.getX();
-        return(ballX < 0 || ballX > (xMax - ball.getSize()));
-    }
-
-    private boolean intersects(ImageView ball, ImageView obstacle) {
-        return ball.getX() < obstacle.getX() + obstacle.getWidth() && ball.getX() + ball.getWidth() > obstacle.getX() && ball.getY() < obstacle.getY() + obstacle.getHeight() && ball.getY() + ball.getHeight() > obstacle.getY();
-    }
-
-    private boolean gameover(){
-        return checkCollisions() || isOutOfBounds();
-
-    }
-
-    private void velocityThreshold(float velocity){
-        float maxVel = 5;
-        if(xVel>maxVel) xVel=maxVel;
-        if(xVel<-maxVel) xVel=-maxVel;
-    }
-
-    private float tiltThreshold(float tilt){
-        if(tilt<-3) return -3;
-        else if(tilt>3) return 3;
-        else return tilt;
-    }
-
-    private void loop() {
-        new Timer().schedule(new TimerTask() {
-            @Override
-            public void run() {runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    if (new Date().getTime() - lastLoopRun > 40) {
-                        addEnemy();
-                        lastLoopRun = new Date().getTime();
-                        Log.d("test", "loop");
-                    }
-                }
-            });
-            }
-        },0, 1000);
-    }
-
-    private void updateBall(float tilt){
-        tilt=tiltThreshold(tilt);
-        float frameTime = 0.666f;
-        xVel += (tilt * frameTime);
-
-        velocityThreshold(xVel);
-        xPos -= (xVel / 2) * frameTime;
-
-        if(!gameover()) {
-            ivBall.setX(xPos + startX);
-            Log.d("xPos", xPos + "");
-        }
-        else{
-            ivBall.setX(xMax/2);
-            Log.d("", "Gameover");
-            GAME_OVER = true;
-        }
-    }
-
-    private class BallSensorListener implements SensorEventListener {
-
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-                if(!GAME_OVER) updateBall(event.values[0]);
-            }
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+    private void initComponents() {
+        btn_startGame = findViewById(R.id.button_startGame);
+        btn_resumeGame = findViewById(R.id.button_resumeGame);
+        leaderboard = findViewById(R.id.leaderboard);
     }
 }
